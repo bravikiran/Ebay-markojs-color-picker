a460488559fabeab5ddfa502b49dad9ba576f836
unknown <ravi.byggari@gmail.com>
2017-09-30 23:22:00 +0200

    markojs color picker example

---
+++ src/components/color-picker/components/color-picker-footer/component.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function isValidHexValue (hexValue) {
+  return /^#[0-9A-F]{6}$/i.test(hexValue);
+}
+
+module.exports = class {
+  onColorSelected (color) {
+    this.emit('colorSelected', color);
+  }
+  onHexInput () {
+    let hexInput = this.getEl('hexInput').value;
+
+    if (!hexInput.startsWith('#')) {
+      hexInput = '#' + hexInput;
+    }
+
+    if (!isValidHexValue(hexInput)) {
+      hexInput = this.input.colors[0];
+    }
+
+    this.emit('colorSelected', hexInput);
+  }
+}
